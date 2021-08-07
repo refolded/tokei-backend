1370a1d151ce0238959623b32b83e41f6e26d506
Ak <abdallahakrab98@gmail.com>
2021-08-07 10:03:18 +0200

    docs: add comments to schema.ts

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -3,11 +3,14 @@ import { loadFilesSync } from "@graphql-tools/load-files";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 
 const path = require("path");
-
+// import typedefs files automatically
 const typesArray = loadFilesSync(path.join(__dirname, "./typedefs"));
+// merge typedefs
 const typeDefs = mergeTypeDefs(typesArray);
 
+// import resolvers files automatically
 const resolversArray = loadFilesSync(path.join(__dirname, "./resolvers"));
+// merge resolvers
 const resolvers = mergeResolvers(resolversArray);
 
 export default makeExecutableSchema({ typeDefs, resolvers });
