bfc0a2257682336985195b4ebdae1711547e4ce8
Ak <abdallahakrab98@gmail.com>
2021-08-19 23:15:32 +0200

    feat: initialize sentry & use sentry plugin
    add SENTRY_DSN to .env

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import { ApolloServer } from "apollo-server-express";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import schema from "./graphql/schema";
 import { context } from "./prisma/context";
+import sentryPlugin from "./graphql/plugins/sentry";
 // Initialize environment variables
 dotenv.config();
 
@@ -13,10 +16,26 @@ const { PORT } = process.env;
 const main = async () => {
   // Create an express app
   const app = express();
+  // Initialize Sentry
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Tracing.Integrations.Express({ app }),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+    // TODO: Adjust in production as recommended by docs
+    tracesSampleRate: 1.0,
+  });
+
   // Create an apollo server
   const apollo = new ApolloServer({
     schema,
     context,
+    plugins: [sentryPlugin],
   });
   // Serve the GraphQL API on the /graphql path
   await apollo.start();
