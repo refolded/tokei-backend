6131a4d1405c8b79a4857632367294bfab3107d8
Ahmad Nader <ahmadnaderx1998@gmail.com>
2021-08-23 19:57:51 +0200

    feat: add initial workspace resolvers

--- graphql/resolvers/workspace.ts
+++ graphql/resolvers/workspace.ts
@@ -1,9 +1,28 @@
 import { PrismaClient } from '@prisma/client'
+import { Workspace } from '../../types/types'
+
 const prisma = new PrismaClient();
 
 export default {
-    Query: {
-      allWorkspaces: () => prisma.workspace.findMany(),
+  Query: {
+    allWorkspaces: () => prisma.workspace.findMany(),
+    getWorkspaceById: (_: any, { id }: { id: any }) =>
+      prisma.workspace.findUnique({
+        where: {
+          id: String(id)
+        }
+      }),
+  },
+  Mutation: {
+    createWorkspace: (_parent:any,args: {createdWorkspace:{type: string, name: string} }) => {
+      const workspace = prisma.workspace.create({
+         data: {
+          type: args.createdWorkspace.type,
+          name: args.createdWorkspace.name
+         } 
+        });
+      return workspace;
     },
-  };
-  
+
+  }
+};
