edf363318dcc7a1777f103bae7dd65fefb4024ba
Ahmad Nader <ahmadnaderx1998@gmail.com>
2021-08-25 18:24:02 +0200

    style: run prettier

--- graphql/typedefs/workspace.ts
+++ graphql/typedefs/workspace.ts
@@ -1,5 +1,5 @@
 import { gql } from "apollo-server-express";
-// import { Workspace as workspace, User } from "../../types/types"; 
+// import { Workspace as workspace, User } from "../../types/types";
 
 export default gql`
   type Workspace {
@@ -19,13 +19,13 @@ export default gql`
     # users: [User]
   }
   type Query {
-    allWorkspaces: [Workspace]!,
+    allWorkspaces: [Workspace]!
     getWorkspaceById(id: String!): Workspace
   }
 
   type Mutation {
-    createWorkspace(createdWorkspace:WorkspaceInput): Workspace,
-    updateWorkspaceById(updatedWorkspace: WorkspaceInput):Workspace,
-    deleteWorkspaceById(id: String!) : Workspace,
+    createWorkspace(createdWorkspace: WorkspaceInput): Workspace
+    updateWorkspaceById(updatedWorkspace: WorkspaceInput): Workspace
+    deleteWorkspaceById(id: String!): Workspace
   }
 `;
