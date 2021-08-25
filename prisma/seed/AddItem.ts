876205c42a9822b2207c26d33d6e89c5a9a521b5
AhmedHassan97 <ahahmed202025@gmail.com>
2021-08-25 21:48:29 +0200

    feat: add item seed

---
+++ prisma/seed/AddItem.ts
@@ -0,0 +1,27 @@
+import faker from "faker";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const addItems = async () => {
+  const itemTitle = await faker.internet.userName();
+  const itemDesc = await faker.commerce.productDescription();
+
+  const projects = await prisma.project.findMany();
+  const randomProject = projects[Math.floor(Math.random() * projects.length)];
+
+  const item = await prisma.item.create({
+    data: {
+      title: itemTitle,
+      description: itemDesc,
+      projects: {
+        connect: {
+          id: randomProject.id,
+        },
+      },
+    },
+  });
+  console.log("🚀 ~ file: AddItem.ts ~ line 24 ~ addItems ~ item", item);
+};
+
+export default addItems;
