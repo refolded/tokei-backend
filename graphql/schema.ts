a1bc4219b8c97c366e7009dbbc311b7a96c1852b
Ahmad Nader <ahmadnaderx1998@gmail.com>
2021-08-27 22:42:44 +0200

    feat: add graphql scalars import

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,6 +1,7 @@
 import { mergeTypeDefs, mergeResolvers } from "@graphql-tools/merge";
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { makeExecutableSchema } from "@graphql-tools/schema";
+import { GraphQLJSON } from "graphql-scalars";
 
 const path = require("path");
 // import typedefs files automatically
@@ -10,6 +11,12 @@ const typeDefs = mergeTypeDefs(typesArray);
 
 // import resolvers files automatically
 const resolversArray = loadFilesSync(path.join(__dirname, "./resolvers"));
+
+// add json type to resolvers
+resolversArray.push({
+  JSON: GraphQLJSON,
+});
+
 // merge resolvers
 const resolvers = mergeResolvers(resolversArray);
 
