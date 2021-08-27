1c2c37895a1298e8c253083aebd45e3784582deb
Amrawy98 <ahmed.m.amrawy@gmail.com>
2021-08-28 01:05:55 +0200

    feat: add update and delete resolvers for property

--- graphql/resolvers/property.ts
+++ graphql/resolvers/property.ts
@@ -16,6 +16,26 @@ export default {
       const property = prisma.property.create({
         data: {
           itemId: args.itemId,
+          value: {},
+        },
+      });
+      return property;
+    },
+    updateProperty: (_parent: any, args: { id: string; value: any }) => {
+      const property = prisma.property.update({
+        where: {
+          id: args.id,
+        },
+        data: {
+          value: args.value,
+        },
+      });
+      return property;
+    },
+    deleteProperty: (_parent: any, args: { id: string }) => {
+      const property = prisma.property.delete({
+        where: {
+          id: String(args.id),
         },
       });
       return property;
