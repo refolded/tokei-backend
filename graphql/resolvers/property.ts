14e8c6e0e771025c343fa0586e9247ed001231cb
Ahmad Nader <ahmadnaderx1998@gmail.com>
2021-08-27 23:35:15 +0200

    feat: add getItemProperties for property resolvers

--- graphql/resolvers/property.ts
+++ graphql/resolvers/property.ts
@@ -1,8 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { IdleTransaction } from "@sentry/tracing";
 
 const prisma = new PrismaClient();
 
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    getAllItemProperties: (_parent: any, args: { itemId: any }) =>
+      prisma.property.findMany({
+        where: {
+          itemId: args.itemId,
+        },
+      }),
+  },
+  // Mutation: {},
 };
