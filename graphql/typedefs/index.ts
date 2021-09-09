2eaf8c863d477810efef6d64c8f9cd60584b0678
Amrawy98 <ahmed.m.amrawy@gmail.com>
2021-09-09 13:01:21 +0200

    style: change PropertyCollection to Property

--- graphql/typedefs/index.ts
+++ graphql/typedefs/index.ts
@@ -1,5 +1,12 @@
 import { gql } from "apollo-server-express";
-import { User, Workspace, Project, Item, PropertyCollection, Block } from "../../types/types";
+import {
+  User,
+  Workspace,
+  Project,
+  Item,
+  Property,
+  Block,
+} from "../../types/types";
 
 export default gql`
   type Query {
