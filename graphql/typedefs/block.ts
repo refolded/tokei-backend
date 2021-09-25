a9ebd3f95245efef05c851bdfceb2f24accc3697
Ahmad Nader <ahmadnaderx1998@gmail.com>
2021-09-25 19:33:12 +0200

    feat: add typedefs for block

---
+++ graphql/typedefs/block.ts
@@ -0,0 +1,22 @@
+import { gql } from "apollo-server-express";
+
+export default gql`
+  scalar JSON
+
+  type Block {
+    id: String
+    content: JSON
+    itemId: String
+    type: String
+  }
+
+  type Query {
+    getBlockById(id: String!): Block 
+  }
+
+  type Mutation {
+    createBlock(content: JSON, itemId: String, type: String): Block
+    updateBlock(content: JSON, itemId: String, type: String): Block
+    deleteBlock(id: String): Block
+  }
+`;
