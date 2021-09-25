922140381a4c0de56bd5d938c41edbdd6da8dc29
Ahmad Nader <ahmadnaderx1998@gmail.com>
2021-09-26 00:22:22 +0200

    feat: add get, create and update to block resolvers

--- graphql/resolvers/block.ts
+++ graphql/resolvers/block.ts
@@ -4,17 +4,40 @@ const prisma = new PrismaClient();
 
 export default {
   Query: {
-    getBlockById: (_parent: any, args:{id: string}) => {
-        prisma.block.findUnique({
-            where: {
-                id: args.id,
-            }
-        });
-    }
-
-    
+    getBlockById: (_parent: any, args: { id: string }) => {
+      const block = prisma.block.findUnique({
+        where: {
+          id: args.id,
+        },
+      });
+      return block;
+    },
   },
   Mutation: {
-   
+    createBlock: (_parent: any, args: { itemId: string; type: string }) => {
+      const block = prisma.block.create({
+        data: {
+          content: {},
+          itemId: args.itemId,
+          type: args.type,
+        },
+      });
+      return block;
+    },
+    updateBlock: (
+      _parent: any,
+      args: { content: object; id: string; type: string }
+    ) => {
+      const block = prisma.block.update({
+        where: {
+          id: args.id,
+        },
+        data: {
+          content: args.content,
+          type: args.type,
+        },
+      });
+      return block;
+    },
   },
 };
